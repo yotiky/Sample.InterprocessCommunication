328e51dceeaa1ca0274b46e6e89675d78823bab2
yotiky <yotiky@gmail.com>
2020-08-05 16:55:00 +0900

    feat: sample codes.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			var sharedMemory = MemoryMappedFile.CreateNew("SharedMemory", 1024);
+
+			while (true)
+			{
+				var str = Console.ReadLine();
+				var data = str.ToCharArray();
+				using (var accessor = sharedMemory.CreateViewAccessor())
+				{
+					accessor.Write(0, data.Length);
+					accessor.WriteArray(sizeof(int), data, 0, data.Length);
+				}
+			}
+		}
+	}
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+using System.Threading;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			var sharedMemory = MemoryMappedFile.OpenExisting("SharedMemory");
+
+			var latestMsg = string.Empty;
+			while (true)
+			{
+				using (var accessor = sharedMemory.CreateViewAccessor())
+				{
+					var size = accessor.ReadInt32(0);
+					var data = new char[size];
+					accessor.ReadArray<char>(sizeof(int), data, 0, data.Length);
+					var str = new string(data);
+					if (latestMsg != str)
+					{
+						latestMsg = str;
+						Console.WriteLine("Data :" + str);
+					}
+				}
+
+				Thread.Sleep(100);
+			}
+		}
+	}
+}
